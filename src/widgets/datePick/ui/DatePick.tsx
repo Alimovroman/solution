517b081f0618dfb0710fb583b90230d38224bb07
Roman <Alimov.ramon@gmail.com>
2024-04-05 03:08:20 +0300

    calculated the days

--- src/widgets/datePick/ui/DatePick.tsx
+++ src/widgets/datePick/ui/DatePick.tsx
@@ -1,11 +1,49 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import style from "./DatePick.module.css";
+import ReactDatePicker from "react-datepicker";
+import { log } from "console";
 
 export const DatePick = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  const dataStart = `${startDate.getFullYear()}/${
+    startDate.getMonth() + 1
+  }/${startDate.getDate()}`;
+  const dataEnd = `${endDate.getFullYear()}/${
+    endDate.getMonth() + 1
+  }/${endDate.getDate()}`;
+
+  let mil = Math.floor(
+    new Date(dataStart).getTime() - new Date(dataEnd).getTime()
+  );
+
+  const dayMs = 86400000;
+
+  useEffect(() => {
+    let seconds = (mil / 1000) | 0;
+
+    let minutes = (seconds / 60) | 0;
+
+    let hours = (minutes / 60) | 0;
+
+    let days = ((hours / 24) * -1) | 0;
+    // hours -= days * 24;
+
+    let weeks = (days / 7) | 0;
+    // days -= weeks * 7;
+    for (let i = 0; i <= days; i++) {
+      if (i === 0) {
+        console.log(new Date(new Date(dataStart).getTime()));
+        // } else if (i === 2) {
+        //   console.log(new Date(new Date(dataStart).getTime() + dayMs));
+      } else {
+        console.log(new Date(new Date(dataStart).getTime() + dayMs * i));
+      }
+    }
+  }, [startDate, endDate]);
 
   return (
     <div>
--- src/widgets/diagram/diagramSlice.ts
+++ src/widgets/diagram/diagramSlice.ts
@@ -19,16 +19,60 @@ const getDataDiagram = createAsyncThunk<
   return { cost: response[value].rub, value };
 });
 
+type DataItem = {
+  data: number[];
+  label: Currency;
+  borderColor: string;
+  backgroundColor?: string;
+  fill: boolean;
+  lineTension: number;
+};
+
+type Currency = "euro" | "usd" | "cny" | "rub";
+
 type InitialState = {
   euro: number | null;
   usd: number | null;
   cny: number | null;
+  data: Data;
 };
 
+type Data = {
+  labels: string[];
+  datasets: DataItem[];
+};
 const initialState: InitialState = {
   euro: null,
   usd: null,
   cny: null,
+  data: {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        label: "euro",
+        borderColor: "#3333ff",
+        fill: true,
+        lineTension: 0.5,
+      },
+      {
+        data: [],
+        label: "usd",
+        borderColor: "#ff3333",
+        backgroundColor: "rgba(255, 0, 0, 0.5)",
+        fill: true,
+        lineTension: 0.5,
+      },
+      {
+        data: [],
+        label: "cny",
+        borderColor: "rgba(128, 300, 200)",
+        backgroundColor: "rgba(128, 300, 200, 0.5)",
+        fill: true,
+        lineTension: 0.5,
+      },
+    ],
+  },
 };
 
 const diagramSlice = createSlice({
